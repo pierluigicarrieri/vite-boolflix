0a34a180e0ea0df372668c2c6fd4287d95e8fdbc
pierluigicarrieri <pierluigi.carrieri.90@gmail.com>
2023-08-28 16:51:47 +0200

    Working on search mechanism

--- src/App.vue
+++ src/App.vue
@@ -16,9 +16,11 @@ export default {
 
   methods: {
 
+
   },
 
   mounted() {
+
 
   }
 
@@ -28,7 +30,28 @@ export default {
 
 <template>
 
-  <div></div>
+<div class="container">
+
+  <form class="d-flex" role="search">
+
+    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" v-model="store.searchText">
+    <button class="btn btn-outline-success">Search</button>
+
+  </form>
+
+  <ul>
+    <li>
+        <div id="info-container">
+          <div id="title-container">aaa</div>
+          <div id="original-title-container">bbb</div>
+          <div id="language-container">ccc</div>
+          <div id="vote-container">ddd</div>
+        </div>
+    </li>
+  </ul>
+
+
+</div>
 
 </template>
 
--- src/store.js
+++ src/store.js
@@ -1,5 +1,26 @@
 import {reactive} from "vue";
+import axios from "axios";
 
 export const store = reactive ({
 
+    searchText : "",
+
+    APIKey : "5ea59b65da75b65f5a104b18d69a4be5",
+
+    dataArray : [],
+
 });
+
+export function fetchData () {
+
+    const url = `"https://api.themoviedb.org/3/search/movie?api_key=${store.APIKey}&query=${store.searchText}"`;
+
+    axios.get(url).then((response) => {
+
+        store.dataArray = response.results;
+
+        console.log(dataArray);
+
+    })
+
+}
