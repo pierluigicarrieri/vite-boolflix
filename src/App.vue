bd63ddf865605e138da4906b2447a67a67c9ef18
pierluigicarrieri <pierluigi.carrieri.90@gmail.com>
2023-08-28 16:05:45 +0200

    First commit, done vite setup

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script>
+
+import {store} from "./store.js";
+
+export default {
+
+  components:{
+
+  },
+
+  data() {
+    return {
+      store,
+    }
+  },
+
+  methods: {
+
+  },
+
+  mounted() {
+
+  }
+
+}
+
+</script>
+
+<template>
+
+  <div></div>
+
+</template>
+
+<style lang="scss">
+
+</style>
