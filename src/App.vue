9db8f030971b24c68876f36bfc68f9ec036c66be
pierluigicarrieri <pierluigi.carrieri.90@gmail.com>
2023-08-29 13:27:49 +0200

    TheMovieContainer component created

--- src/App.vue
+++ src/App.vue
@@ -44,18 +44,7 @@ export default {
 
   <TheHeader></TheHeader>
 
-  <ul v-for="movie in store.movieDataArray">
-    <li>
-        <div id="info-container">
-          <div id="title-container">{{ movie.title }}</div>
-          <div id="original-title-container">{{ movie.original_title }}</div>
-          <div id="language-container">
-            <img :src="`https://flagsapi.com/${flagGenerator(movie.original_language)}/flat/64.png`" :alt="`${movie.original_language}`">
-          </div>  
-          <div id="vote-container">{{ movie.vote_average }}</div>
-        </div>
-    </li>
-  </ul>
+  <TheMovieContainer></TheMovieContainer>
 
   <ul v-for="TVSeries in store.TVDataArray">
     <li>
--- src/components/TheMovieContainer.vue
+++ src/components/TheMovieContainer.vue
@@ -1,12 +1,52 @@
 <script>
 
+import {store, flagGenerator} from "../store.js";
+
 export default {
+
+data() {
+
+    return {
+
+        store,
+
+    }
+
+},
+
+methods: {
+
+    flagGenerator
+
+},
+
+mounted() {
+},
 
 }
 
 </script>
 
-<template></template>
+<template>
+
+    <div class="container">
+
+        <ul v-for="movie in store.movieDataArray">
+            <li>
+                <div id="info-container">
+                <div id="title-container">{{ movie.title }}</div>
+                <div id="original-title-container">{{ movie.original_title }}</div>
+                <div id="language-container">
+                    <img :src="`https://flagsapi.com/${flagGenerator(movie.original_language)}/flat/64.png`" :alt="`${movie.original_language}`">
+                </div>  
+                <div id="vote-container">{{ movie.vote_average }}</div>
+                </div>
+            </li>
+        </ul>
+
+    </div>
+
+</template>
 
 <style lang="scss" scoped>
 
