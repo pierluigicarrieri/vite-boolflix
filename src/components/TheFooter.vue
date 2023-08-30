f6ce1984bae4616388936e1b96c6b86334c05eeb
pierluigicarrieri <pierluigi.carrieri.90@gmail.com>
2023-08-30 10:34:21 +0200

    Boolean credits added to footer

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -7,7 +7,9 @@ export default {
 </script>
 
 <template>
-    <footer class="py-5 bg-dark"></footer>
+    <footer class="py-5 bg-dark">
+        <h4 class="text-danger text-center">Courtesy of Boolean Academy&reg;</h4>
+    </footer>
 </template>
 
 <style lang="scss" scoped>
